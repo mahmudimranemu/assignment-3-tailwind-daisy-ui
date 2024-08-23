bc1199396579f3b3a75ddb35512655371b4a0898
Mahmud <mahmudimranemu@gmail.com>
2024-08-23 12:32:37 +0600

    DaisyUI setup and hero section completed

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
